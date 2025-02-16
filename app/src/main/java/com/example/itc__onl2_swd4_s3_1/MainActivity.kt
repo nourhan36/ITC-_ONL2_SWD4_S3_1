d3835c106529892dc770c7e671e0a95ec29591cd
manar <manarkhaledx@outlook.com>
2025-02-16 23:01:11 +0200

    x

--- app/src/main/java/com/example/itc__onl2_swd4_s3_1/MainActivity.kt
+++ app/src/main/java/com/example/itc__onl2_swd4_s3_1/MainActivity.kt
@@ -18,12 +18,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            ITC_ONL2_SWD4_S3_1Theme {
-                // A surface container using the 'background' color from the theme
-print("x")
-            }
-        }
+
     }
 }
 
