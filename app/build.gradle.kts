16bbea684e0550090c50ba92120986bc80772cb1
nourhan36 <nourhanibrahim2004@gmail.com>
2025-03-09 00:59:54 +0200

    Progress Page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.itc__onl2_swd4_s3_1"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/com/example/itc__onl2_swd4_s3_1/ui/theme/Theme.kt
+++ app/src/main/java/com/example/itc__onl2_swd4_s3_1/ui/theme/Theme.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -18,9 +19,24 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color(0xFF162E42),
+    onPrimary = Color(0xFFFFFFFF),
+    primaryContainer = Color(0xFF162E42).copy(alpha = 0.2f),
+    onPrimaryContainer = Color(0xFF162E42),
+
+    secondary = Color(0xFF407EA2),
+    onSecondary = Color(0xFFFFFFFF),
+    secondaryContainer = Color(0xFF407EA2).copy(alpha = 0.2f),
+    onSecondaryContainer = Color(0xFF407EA2),
+
+    background = Color(0xFFE7DFCF),
+    onBackground = Color(0xFF162E42),
+
+    surface = Color(0xFFF5F0E6),
+    onSurface = Color(0xFF162E42),
+
+    error = Color(0xFFD32F2F),
+    onError = Color(0xFFFFFFFF)
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
