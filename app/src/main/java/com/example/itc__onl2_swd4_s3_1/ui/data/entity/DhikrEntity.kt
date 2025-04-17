77c138f19922c87ad12b9c2b1e80b2fa7b403cb9
manar <manarkhaledx@outlook.com>
2025-04-17 23:34:47 +0200

    dhikr& salah Datalayer added

---
+++ app/src/main/java/com/example/itc__onl2_swd4_s3_1/ui/data/entity/DhikrEntity.kt
@@ -0,0 +1,14 @@
+package com.example.itc__onl2_swd4_s3_1.ui.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "dhikr_table")
+data class DhikrEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val category: String,
+    val count: Int,
+    val translation: String,
+    val content: String,
+    val description: String
+)
--- app/src/main/java/com/example/itc__onl2_swd4_s3_1/ui/data/entity/SalahEntity.kt
+++ app/src/main/java/com/example/itc__onl2_swd4_s3_1/ui/data/entity/SalahEntity.kt
@@ -1,4 +1,12 @@
 package com.example.itc__onl2_swd4_s3_1.ui.data.entity
 
-class SalahEntity {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDate
+
+@Entity(tableName = "salah_records")
+data class SalahEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val date: LocalDate,
+    val prayers: String
+)
