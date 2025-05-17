4c8d5a0cc7cb1370f66dc13d050f65d58e16afb2
manar <manarkhaledx@outlook.com>
2025-05-17 03:40:48 +0300

    folder modified

--- app/src/main/java/com/example/itc__onl2_swd4_s3_1/ui/data/entity/CompletedDayEntity.kt
+++
@@ -1,9 +0,0 @@
-package com.example.itc__onl2_swd4_s3_1.ui.data.entity
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "completed_days")
-data class CompletedDayEntity(
-    @PrimaryKey val date: String // format yyyy-MM-dd
-)
--- app/src/main/java/com/example/itc__onl2_swd4_s3_1/ui/ui/habitSelector/Habit.kt
+++
@@ -1,3 +0,0 @@
-package com.example.itc__onl2_swd4_s3_1.ui.ui.habitSelector
-
-data class Habit(val title: String, val iconRes: Int)
